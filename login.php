6b2126f73cc6d9b63289af8e192cfc44be979e4c
caioresendede <caioresende14@gmail.com>
2024-08-16 14:43:21 -0300

    commit tcc caio cadastro e login

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php include "conecta.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head> <link rel="stylesheet" type="text/css" href="cssindex.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="font-size: 20px;background-color: rgb(33, 25, 51); font-family:monospace; color: #a9dd0c;">
+
+<H1 id="mensageminicial">PROJETO</H1>
+<div class="menu">
+<ul style="list-style-type: none; width: 100%;">
+    <li class="menu"><a href=""> HOME </li></a>
+    <li class="menu"><a href=""> TIMES </li></a>
+    <li class="menu"><a href=""> AGENDA </li></a>
+    <li class="menu"><a href=""> SOBRE </li></a>
+    <li class="menu"><a href=""> NOTÍCIAS </li></a>
+    <li class="menu"><a href=""> DESTAQUES </li></a>
+    <li class="menu"><a href="cadastro.php"> CADASTRO </li></a></div>
+
+ 
+    <h2 style="text-align: center;"> Login </h2> <br>
+
+    <label> Email ou Discord </label><br>
+    <input type="text"> <br>
+    <label> Senha </label><br>
+    <input type="password"> <br>
+
+    <a href="Login"> Não tem cadastro? Entre aqui! </a>
+
+
+
+
+
+    
+</body>
+</html>
